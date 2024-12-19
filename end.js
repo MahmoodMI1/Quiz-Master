17a7849b6d7c1cadaece181914d33bda6c92b96a
MahmoodMI1 <mahmood.idelbi1@icloud.com>
2024-12-19 11:59:14 -0500

    Added high score page

--- end.js
+++ end.js
@@ -2,8 +2,11 @@ const username = document.getElementById('username');
 const saveScoreBtn = document.getElementById('saveScoreBtn');
 const finalScore = document.getElementById('finalScore');
 
-
 const mostRecentScore = localStorage.getItem('mostRecentScore');
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];// if there is nothing in local (null) return []
+
+const MAX_HIGH_SCORES = 5;
+
 finalScore.innerText = mostRecentScore;
 
 // Enable button depending on the input
@@ -13,4 +16,17 @@ username.addEventListener('keyup', () =>{
 
 saveHighScore = (e) => {
     e.preventDefault();
-}
+
+    const score = {
+        score: mostRecentScore,
+        name: username.value
+    };
+    highScores.push(score);// add to the list
+    highScores.sort ((a,b) => { 
+        return b.score - a.score // if b score > a score put b before a
+    })
+    highScores.splice(5) // Cut everything after fifth index
+
+    localStorage.setItem("highScores", JSON.stringify(highScores));
+    window.location.assign('https://mahmoodmi1.github.io/Quiz-Master/');
+};
